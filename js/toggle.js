4bc8d3f691d50e969adb7ff12a86c4d52e7a563f
akroms <Akromiphone7.90@gmail.com>
2021-09-22 20:33:42 +0500

    js toggle animation

--- js/toggle.js
+++ js/toggle.js
@@ -1,15 +1,64 @@
-let toggleStyle = ` 
+let open = ` 
     @media (max-width:768px) {
         .navbar {
             align-items: flex-start; 
         }
+
+        @keyframes moveRight {
+            from {
+                left: -130%;
+            }
+            to {
+                left: 0%;
+            }
+          }
+
         .navbar-list {
             display:block;
+            position: relative;
+
+            animation-name: moveRight;
+            animation-duration: 750ms;
         }
     
         .navbar-toggle {
             margin-top:0.5em;
         }
+    }
+`;
+
+let close = `
+@media (max-width:768px) {
+    .navbar {
+        align-items: flex-start; 
+    }
+
+    @keyframes moveLeft {
+        from {
+            left: 0%;
+        }
+        to {
+            left: -140%; 
+        }
+    }
+
+    .navbar-list {
+        display:block;
+        position:relative;
+
+        animation-name: moveLeft;
+        animation-duration: 750ms;
+        left: -120%; 
+    }
+
+    .navbar-toggle {
+        margin-top:0.5em;
+    }
+}   
+`;
+let displayNone = `
+    .navbar-list {
+        display:none;
     }
 `;
 
@@ -19,7 +68,12 @@ let style = document.head.appendChild(createElement);
 let toggle = document.querySelector(".navbar-toggle");
 
 toggle.addEventListener("click", () => {
-    style.innerHTML === toggleStyle
-        ? (style.innerHTML = "")
-        : (style.innerHTML = toggleStyle);
+    if (style.innerHTML === open) {
+        style.innerHTML = close;
+        setTimeout(() => {
+            style.innerHTML = displayNone;
+        }, 500);
+    } else {
+        style.innerHTML = open;
+    }
 });
